2177ddcef4197b2e000f621dc3f0a9f88495cddf
DEREVGEN <96948245+DEREVGEN@users.noreply.github.com>
2023-08-11 01:12:49 +0900

    swagger 이름 추가 및 변경

--- src/main/java/site/chagok/server/member/controller/MemberController.java
+++ src/main/java/site/chagok/server/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package site.chagok.server.member.controller;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,8 @@ import site.chagok.server.member.exception.NickNameExistsException;
 import site.chagok.server.member.repository.MemberRepository;
 import site.chagok.server.member.service.MemberService;
 
+
+@Api(tags ="사용자 데이터 관리")
 @RestController
 @RequestMapping("/member")
 @RequiredArgsConstructor
@@ -26,7 +29,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/nickname")
-    @ApiOperation(value="닉네임 업데이트", notes = "이미 존재하는 닉네임이라면, 409 error")
+    @ApiOperation(value="닉네임 업데이트", notes = "파라미터 nickname = 변경할 닉네임, 이미 존재하는 닉네임이라면, 409 error")
     public ResponseEntity changeNickName(@RequestParam("nickname")String nickName) {
 
         try {
@@ -39,7 +42,7 @@ public class MemberController {
     }
 
     @GetMapping("/scrap")
-    @ApiOperation(value = "게시글 스크랩", notes = "catergory = ( study, project, contest) , id = 게시글 삭제 id")
+    @ApiOperation(value = "게시글 스크랩", notes = "파라미터  catergory = ( study, project, contest ) , 파라미터 id = 스크랩 삭제할 게시글 id")
     public ResponseEntity scrapBoard(@RequestParam("category") String category, Long id) {
         memberService.addBoardScrap(category, id);
 
