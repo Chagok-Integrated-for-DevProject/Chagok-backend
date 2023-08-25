b2dba435973dc090140e0ac8e6712b29ffb8155b
DEREVGEN <96948245+DEREVGEN@users.noreply.github.com>
2023-08-25 12:24:01 +0900

    cors 설정 - chagok.site

--- src/main/java/site/chagok/server/security/config/SecurityConfig.java
+++ src/main/java/site/chagok/server/security/config/SecurityConfig.java
@@ -1,10 +1,6 @@
 package site.chagok.server.security.config;
 
 import lombok.RequiredArgsConstructor;
-import org.jose4j.jwk.RsaJsonWebKey;
-import org.jose4j.jwk.RsaJwkGenerator;
-import org.jose4j.lang.JoseException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,14 +9,20 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import site.chagok.server.security.constants.SecutiryHeader;
 import site.chagok.server.security.filter.JwtHeaderCheckingFilter;
 import site.chagok.server.security.service.ChagokOAuth2UserService;
 import site.chagok.server.security.service.OAuth2SuccessHandler;
-
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @RequiredArgsConstructor
@@ -47,12 +49,29 @@ public class SecurityConfig {
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // stateless 세팅
                 .addFilterAfter(jwtHeaderCheckingFilter, BasicAuthenticationFilter.class) // jwt 헤더 검사
                 .csrf().disable() // test
+                .cors(withDefaults())
                 .httpBasic().disable();
 
         return http.build();
     }
 
 
+    // chagok.site 간 cors config
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(Arrays.asList("https://chagok.site", "https://localhost:3000", "http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "DELETE", "PUT"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of(SecutiryHeader.JWT_HEADER)); // custom 설정 중 해당 헤더만 허용
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+    // 로그인 방식 설정
     public class AuthenticationEntryPoint extends LoginUrlAuthenticationEntryPoint {
         public AuthenticationEntryPoint()  {
             super("");
