7d11956320b19ec85f2128b6513c221fae1e48d3
DEREVGEN <96948245+DEREVGEN@users.noreply.github.com>
2023-10-07 23:40:58 +0900

    spring doc api - base 주소 코드 추가

--- src/main/java/site/chagok/server/ChagokApplication.java
+++ src/main/java/site/chagok/server/ChagokApplication.java
@@ -1,11 +1,14 @@
 package site.chagok.server;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@OpenAPIDefinition(servers = {@Server(url = "/")})
 public class ChagokApplication {
 
 	public static void main(String[] args) {
